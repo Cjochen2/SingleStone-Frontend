a10cc11f92a574cc93d910bdcbd7c4fa8b8f4f99
Curtis Jochen <curtis.jochen@gmail.com>
2020-06-15 15:51:18 -0400

    Added API call using axios. Data logs to the console.

--- client/src/components/CardPanel/index.js
+++ client/src/components/CardPanel/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './index.css';
 import Card from '../Card'
 
 function CardPanel() {
+
+    useEffect(() =>{
+        axios.get('https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge')
+            .then((response) =>{
+                console.log(response)
+            })
+    });
 
     return (
         <div>
