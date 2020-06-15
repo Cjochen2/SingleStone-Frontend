2118d5575fe9158524ebbf9156c949f21912e677
Curtis Jochen <curtis.jochen@gmail.com>
2020-06-15 16:44:03 -0400

    Updated the API call. Set up the card template and passed props. Need to set up filter to grab proper body text

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -2,55 +2,20 @@ import React from 'react';
 import './index.css';
 
 
-function Card() {
+function Card({stepNumber, title, body}) {
 
     return (
-        <div className='Container'>
             <div className='Card-container'>
                 <h1 className='Card-number'>
-                    01
+                    {stepNumber}
             </h1>
                 <h4 className='Card-title'>
-                    FILL OUT A PROFILE
+                    {title}
             </h4>
                 <p className='Card-content'>
-                    We only want you to get games and gear that you'll love.
+                    {body}
             </p>
             </div>
-            <div className='Card-container'>
-                <h1 className='Card-number'>
-                    01
-            </h1>
-                <h4 className='Card-title'>
-                    FILL OUT A PROFILE
-            </h4>
-                <p className='Card-content'>
-                    We only want you to get games and gear that you'll love.
-            </p>
-            </div>
-            <div className='Card-container'>
-                <h1 className='Card-number'>
-                    01
-            </h1>
-                <h4 className='Card-title'>
-                    FILL OUT A PROFILE
-            </h4>
-                <p className='Card-content'>
-                    We only want you to get games and gear that you'll love.
-            </p>
-            </div>
-            <div className='Card-container'>
-                <h1 className='Card-number'>
-                    01
-            </h1>
-                <h4 className='Card-title'>
-                    FILL OUT A PROFILE
-            </h4>
-                <p className='Card-content'>
-                    We only want you to get games and gear that you'll love.
-            </p>
-            </div>
-        </div>
     )
 }
 
--- client/src/components/CardPanel/index.js
+++ client/src/components/CardPanel/index.js
@@ -1,24 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './index.css';
 import Card from '../Card'
 
 function CardPanel() {
+    const [data, setData] = useState([]);
+    let objArray;
 
-    useEffect(() =>{
+    useEffect(() => {
         axios.get('https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge')
-            .then((response) =>{
-                console.log(response)
+            .then((response) => {
+                console.log(response);
+                // Assigns array to variable
+                objArray = response.data;
+                // Sorts objects based on stepNumber property
+                objArray.sort((a, b) => (a.stepNumber > b.stepNumber) ? 1 : -1);
+                console.log(objArray);
+                setData(objArray);
             })
-    });
+
+    }, [])
+
+
 
     return (
         <div>
             <div className='Panel-title'>
                 How It Works
             </div>
-            <div>
-                <Card />
+            <div className='Panel-cards-container'>
+                {data.map((obj, index) => <Card key={index} stepNumber={obj.stepNumber} title={obj.versionContent[0].title} body={obj.versionContent[0].body} />)}
             </div>
         </div>
     )
