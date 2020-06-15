629be863b92fde1990c5a8acbe6b2bedcc0f597a
Curtis Jochen <curtis.jochen@gmail.com>
2020-06-15 15:36:28 -0400

    Hard coded 4 card designs for styling purposes. Styled the four cards.  Need to set up update cards to be template for handling API call, as well as build the API call. Main image still needs editing and a div added with content

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import './index.css';
 
+
 function Card() {
 
-    return(
-        <div>
-            Card
+    return (
+        <div className='Container'>
+            <div className='Card-container'>
+                <h1 className='Card-number'>
+                    01
+            </h1>
+                <h4 className='Card-title'>
+                    FILL OUT A PROFILE
+            </h4>
+                <p className='Card-content'>
+                    We only want you to get games and gear that you'll love.
+            </p>
+            </div>
+            <div className='Card-container'>
+                <h1 className='Card-number'>
+                    01
+            </h1>
+                <h4 className='Card-title'>
+                    FILL OUT A PROFILE
+            </h4>
+                <p className='Card-content'>
+                    We only want you to get games and gear that you'll love.
+            </p>
+            </div>
+            <div className='Card-container'>
+                <h1 className='Card-number'>
+                    01
+            </h1>
+                <h4 className='Card-title'>
+                    FILL OUT A PROFILE
+            </h4>
+                <p className='Card-content'>
+                    We only want you to get games and gear that you'll love.
+            </p>
+            </div>
+            <div className='Card-container'>
+                <h1 className='Card-number'>
+                    01
+            </h1>
+                <h4 className='Card-title'>
+                    FILL OUT A PROFILE
+            </h4>
+                <p className='Card-content'>
+                    We only want you to get games and gear that you'll love.
+            </p>
+            </div>
         </div>
     )
 }
--- client/src/components/CardPanel/index.js
+++ client/src/components/CardPanel/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './index.css';
 import Card from '../Card'
 
-function CardPanel () {
+function CardPanel() {
 
     return (
         <div>
-            <Card />
+            <div className='Panel-title'>
+                How It Works
+            </div>
+            <div>
+                <Card />
+            </div>
         </div>
     )
 }
