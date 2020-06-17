a2e4bbf8a6b44c0e64afb5cb30e871e24125be74
Curtis Jochen <curtis.jochen@gmail.com>
2020-06-16 21:28:56 -0400

    Added button transition. Made the button link to an external page

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -16,7 +16,9 @@ function Main() {
             <div id='Content-3'>
                 What's the best way to shop for the latest video games and peripherals? How about never shopping at all? You'll get new stuff on your doorstep-every month.
             </div>
-            <button id='Get-started'>GET STARTED</button>
+            <a href="https://cjochen2.github.io/Portfolio/" target="_blank" rel="noopener noreferrer">
+                <button id='Get-started'>Get Started</button>
+            </a>
         </div>
     )
 }
