1d29a62dda903fe170c1d08b4ee8cd32068d4ef5
Curtis Jochen <curtis.jochen@gmail.com>
2020-06-16 20:39:53 -0400

    Updated sorting functions to remove extra data. Set up the main content. Need to work on touch ups and responsive aspects of the application.

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -9,9 +9,9 @@ function Card({stepNumber, title, body}) {
                 <h1 className='Card-number'>
                     0{stepNumber}
             </h1>
-                <h4 className='Card-title'>
+                <h3 className='Card-title'>
                     {title}
-            </h4>
+            </h3>
                 <p className='Card-content'>
                     {body}
             </p>
--- client/src/components/CardPanel/index.js
+++ client/src/components/CardPanel/index.js
@@ -5,7 +5,21 @@ import Card from '../Card'
 
 function CardPanel() {
     const [data, setData] = useState([]);
-    
+    // Functions used to execute the sorting of data
+    const dateSorter = (dates) => {
+        dates.forEach(obj => obj.versionContent.sort((a, b) => (b.effectiveDate > a.effectiveDate) ? 1 : -1))
+    };
+
+    const stepSorter = (steps) => {
+        steps.sort((a, b) => (a.stepNumber > b.stepNumber) ? 1 : -1);
+    }
+
+    const dataSorter = (data) => {
+        data.forEach(obj => {
+            obj.versionContent = obj.versionContent[0];
+        })
+
+    }
 
     useEffect(() => {
         axios.get('https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge')
@@ -13,23 +27,21 @@ function CardPanel() {
                 let objArray;
                 // Assigns array to variable
                 objArray = response.data;
+
                 // Sorts objects based on stepNumber property
                 stepSorter(objArray)
+
                 // Sorts versionContent based on effective Dates
                 dateSorter(objArray);
+
+                // Removes old versionContent by setting the property equal to the first index of the array
+                dataSorter(objArray);
+
                 // Updates and sets the State to the now sorted JSON data
                 setData(objArray);
             })
 
     }, [])
-
-    const dateSorter = (dates) => {
-        dates.forEach(obj => obj.versionContent.sort((a, b) => (b.effectiveDate >  a.effectiveDate) ? 1 : -1))
-    };
-
-    const stepSorter = (steps) => {
-        steps.sort((a, b) => (a.stepNumber > b.stepNumber) ? 1 : -1);
-    }
 
     return (
         <div className='Panel-container'>
@@ -37,7 +49,7 @@ function CardPanel() {
                 How It Works
             </div>
             <div className='Panel-cards-container'>
-                {data.map((obj, index) => <Card key={index} stepNumber={obj.stepNumber} title={obj.versionContent[0].title} body={obj.versionContent[0].body} />)}
+                {data.map((obj, index) => <Card key={index} stepNumber={obj.stepNumber} title={obj.versionContent.title} body={obj.versionContent.body} />)}
             </div>
         </div>
     )
